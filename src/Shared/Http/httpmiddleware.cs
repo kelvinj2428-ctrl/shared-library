4434ebfddfcea69873211b0aeb48c61944f6b2a5
kelvin2428-ctrl <kelvinj2428@gmail.com>
2026-04-14 12:56:48 -0400

    Add User feature

---
+++ src/Shared/Http/httpmiddleware.cs
@@ -0,0 +1,4 @@
+namespace Shared.Http;
+using System.Collections;
+using System.Net;
+public delegate Task HttpMiddleware(HttpListenerRequest req, HttpListenerResponse res, Hashtable props, Func<Task> next);
